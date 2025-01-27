1000569fb9cd4538bce374212abd74ed4e1aa8f5
MIAACTON-STUDENT <G00428601@atu.ie>
2025-01-27 12:39:51 +0000

    Added new Dessert class and included total cost of order

--- src/main/java/ie/atu/week2sem2/JustApp.java
+++ src/main/java/ie/atu/week2sem2/JustApp.java
@@ -8,25 +8,31 @@ public class JustApp {
         ArrayList<MenuItem> items = new ArrayList<>();
         System.out.println("Welcome to Just App!");
         System.out.println("This is your order...");
-        //Create an order
+        // Create an order
         Burger cheeseBurger = new Burger("Cheeseburger", 1.99, "Tasty");
-        Burger veggie = new Burger("Veggie Burger", 2.99, "Very Tasty!");
+        Burger veggie = new Burger("Veggie Burger", 2.99, "Delicious!");
         Pizza tuna = new Pizza("Tuna Pizza", 8.99, "Surprisingly nice");
-        Fries curry = new Fries("Curry Fries", 2.99, "Again very nice");
+        Fries curry = new Fries("Curry Fries", 2.99, "Lovely");
+        Dessert applePie = new Dessert("Apple Pie", 4.99, "So sweet!");
 
 
-        //List all of the food items like burgers, pizzas, fries, etc
+        // List all of the food items like burgers, pizzas, fries, etc
         items.add(cheeseBurger);
         items.add(veggie);
         items.add(tuna);
         items.add(curry);
+        items.add(applePie);
 
-        //Display details to the user
+        // Display details to the user
+        double totalCost = 0;
         for(MenuItem item : items)
         {
             System.out.println(item.getName());
             System.out.println(item.getDescription());
             System.out.println(item.getPrice());
+            // Calculate and display the total cost of the order
+            totalCost += item.getPrice();
         }
+        System.out.println("Total Cost: €" + totalCost);
     }
 }
